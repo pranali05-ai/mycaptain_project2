2f7f0d5bff3254c402b37259063f86fc63c6177b
pranali05-ai <73159460+pranali05-ai@users.noreply.github.com>
2021-11-30 09:11:09 -1000

    Add files via upload

---
+++ task 2 no. in range.py
@@ -0,0 +1,30 @@
+# Python program to print all positive Numbers in a range
+
+#1] list of numbers using for loop
+list1 = [12, -7, 5, 64,-14]
+
+# iterating each number in list
+for num in list1:
+	
+# checking condition
+    if num >= 0:
+        break
+    print(num, end = " ")
+
+
+
+#2] list of numbers
+list2 = [12, 14, -95, 3]
+num = 0
+
+# using while loop	
+while(num < len(list2)):
+	
+	# checking condition
+	if list2[num] >= 0:
+	   print(list2[num], end = [""])
+	
+	# increment num
+	num += 1
+
+	
